e370f2289ce33cfdfcf6e0db3c782b2048c89c97
Peter Imini <peterimini2@gmail.com>
2026-06-29 01:09:46 +0100

    correct footer.js in services pages

---
+++ js/menu-2.js
@@ -0,0 +1,18 @@
+       fetch("../header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+
+        // Now the elements exist
+        const mobileBtn = document.getElementById("mobile-cta");
+        const nav = document.querySelector(".top-nav");
+        const mobileBtnExit = document.getElementById("mobile-exit");
+
+        mobileBtn.addEventListener("click", () => {
+            nav.classList.add("menu-btn");
+        });
+
+        mobileBtnExit.addEventListener("click", () => {
+            nav.classList.remove("menu-btn");
+        });
+    });
