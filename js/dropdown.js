0fdf929e304750934544f4ebdd5183b46fcf10df
Peter Imini <peterimini2@gmail.com>
2026-09-03 18:43:02 +0100

    create dropdown function and add dropdown to faqs.html

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,17 +1,23 @@
 const buttons = document.querySelectorAll('.faqs-btn');
+const faqsBtn = document.querySelectorAll('.faqs-btn-2');
 
-buttons.forEach((button) => {
+dropDown(buttons);
+dropDown(faqsBtn);
+
+function dropDown(buttons) {
+  buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const answer = button.parentElement.nextElementSibling;
-    const isOpen = answer.classList.contains('show');
+    const openFaq = answer.classList.contains('show');
 
     buttons.forEach((item) => {
       const otherAnswer = item.parentElement.nextElementSibling;
       otherAnswer.classList.remove('show');
     });
     
-    if (!isOpen) {
+    if (!openFaq) {
       answer.classList.add('show');
     }
   });
 });
+}
--- js/track.js
+++ js/track.js
@@ -1,11 +1,21 @@
 const input = document.querySelector('.text-box');
 const trackBtn = document.querySelector('.track-btn');
 
+function trackId() {
+    if (input.value === '') {
+      alert('Please input tracking ID');
+    } else if (input.value <= 50) {
+      alert('Tracking in process.');
+    } else
+      alert('Tracking ID not found. Please check and try again.')
+};
+  
 trackBtn.addEventListener("click", () => {
-  if (input.value <= 50) {
-    alert('Tracking in process.');
-  } else if (input.value = "") {
-    alert('Please input tracking ID');
-  } else
-    alert('Tracking ID not found. Please check and try again.')
+  trackId();
+});
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    trackId();
+  }
 });
